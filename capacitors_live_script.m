92d2d382719b123228de359175c185f73f763e66
lolo23jhon <lolo23jhon@gmail.com>
2020-04-24 12:00:20 -0700

    Updated live script.

--- capacitors_live_script.m
+++ capacitors_live_script.m
@@ -1,29 +1,30 @@
 clear all;
 clc;
 
-V0 = 20;    % (V)Voltaje inicial
-R = 1000;   % (Omhs) Resistencia
-C = 1e-6;   % (F) Capacitancia
+V0 = 20;    % (V)       Voltaje inicial
+R = 1000;   % (Omhs)    Resistencia
+C = 1e-6;   % (F)       Capacitancia
+RC = R*C;   % (s)       Constante RC
 
-RC = R*C;
+step = 1e-9;% Paso de generación de datos 
 
-% Ciclo de carga
-V = @(t, V0, RC) V0*(1-exp(-t./RC));
+% Modelo de voltaje de carga
+V_charge = @(t, V0, RC) V0*(1-exp(-t./RC));
 
-t = 0:1e-9:5*RC;
-v = V(t, V0, RC);
+% Modelo voltaje en descarga
+V_discharge = @(t, V0, RC) V0*(exp(-t./RC));
+
+% Parametrización de t
+t_cycle = 0:step:5*RC;
+t = [t_cycle, t_cycle + t_cycle(end)];
+
+% Parametrización de voltaje
+v = [V_charge(t_cycle, V0, RC), V_discharge(t_cycle,V0, RC)];
 
 figure(1);
-plot(v,t,"Color","red");
+plot(t,v,'.', "Color","red");
+grid on;
 
-data = table2array(readtable("sim_1_data.csv", "NumHeaderLines",1));
-x = data(:,1);
-y = data(:,2);
-
-
-
-figure(2);
-plot(x,y,"Color","green");
 
 
 
