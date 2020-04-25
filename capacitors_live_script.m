a3b5abfb96f937583a09fafd3217c9c65a00d377
lolo23jhon <lolo23jhon@gmail.com>
2020-04-24 20:18:24 -0700

    Updated scripts and added gifs for voltage.

--- capacitors_live_script.m
+++ capacitors_live_script.m
@@ -1,12 +1,30 @@
+
+
+%## Circuito 1
 clear all;
+close all;
 clc;
+% ------------- Parámetros--------------------%
 
 V0 = 20;    % (V)       Voltaje inicial
 R = 1000;   % (Omhs)    Resistencia
 C = 1e-6;   % (F)       Capacitancia
 RC = R*C;   % (s)       Constante RC
 
-step = 1e-9;% Paso de generación de datos 
+step = 1e-9;% Paso de generación de datos
+
+fig_num = 1; % Número defigura
+fig_title = sprintf("Circuit 1: V %.3f V, RC: %.3f s",V0, RC);
+x_label = "Time (s)";
+y_label = "Voltage (V)";
+plot_args = {"-b", "Color", "red", "LineWidth", 2};
+
+is_create_gif = true;
+filename = "circuit_1.gif";
+gif_step = 0.5e-4;
+gif_line_args = {"Color", "red", "LineWidth", 2};
+
+% --------------------------------------------%
 
 % Modelo de voltaje de carga
 V_charge = @(t, V0, RC) V0*(1-exp(-t./RC));
@@ -21,10 +39,70 @@ t = [t_cycle, t_cycle + t_cycle(end)];
 % Parametrización de voltaje
 v = [V_charge(t_cycle, V0, RC), V_discharge(t_cycle,V0, RC)];
 
-figure(1);
-plot(t,v,'.', "Color","red");
+figure(fig_num);
+plot(t,v,plot_args{:});
 grid on;
+title(fig_title);
+xlabel(x_label);
+ylabel(y_label);
 
+if is_create_gif
+    t_gif_cycle = 0:gif_step:5*RC;
+    t_gif = [t_gif_cycle, t_gif_cycle + t_gif_cycle(end)];
+    v_gif = [V_charge(t_gif_cycle, V0, RC), V_discharge(t_gif_cycle,V0, RC)];
+    plot2gif(fig_num+1, t_gif,v_gif, fig_title, x_label, y_label, filename, gif_line_args);
+end
 
+%## Circuito 2
+clear all;
+close all;
+clc;
+% ------------- Parámetros--------------------%
 
+V0 = 10;    % (V)       Voltaje inicial
+R = 1000;   % (Omhs)    Resistencia
+C = 1e-6;   % (F)       Capacitancia
+RC = R*C;   % (s)       Constante RC
+
+step = 1e-9;% Paso de generación de datos
+
+fig_num = 2; % Número defigura
+fig_title = sprintf("Circuit 2: V %.3f V, RC: %.3f s",V0, RC);
+x_label = "Time (s)";
+y_label = "Voltage (V)";
+plot_args = {"-b", "Color", "red", "LineWidth", 2};
+
+is_create_gif = true;
+filename = "circuit_2.gif";
+gif_step = 0.5e-4;
+gif_line_args = {"Color", "red", "LineWidth", 2};
+
+% --------------------------------------------%
+
+% Modelo de voltaje de carga
+V_charge = @(t, V0, RC) V0*(1-exp(-t./RC));
+
+% Modelo voltaje en descarga
+V_discharge = @(t, V0, RC) V0*(exp(-t./RC));
+
+% Parametrización de t
+t_cycle = 0:step:5*RC;
+t = [t_cycle, t_cycle + t_cycle(end)];
+
+% Parametrización de voltaje
+v = [V_charge(t_cycle, V0, RC), V_discharge(t_cycle,V0, RC)];
+
+figure(fig_num);
+plot(t,v,plot_args{:});
+grid on;
+title(fig_title);
+xlabel(x_label);
+ylabel(y_label);
+
+if is_create_gif
+    t_gif_cycle = 0:gif_step:5*RC;
+    t_gif = [t_gif_cycle, t_gif_cycle + t_gif_cycle(end)];
+    v_gif = [V_charge(t_gif_cycle, V0, RC), V_discharge(t_gif_cycle,V0, RC)];
+    plot2gif(fig_num+1, t_gif,v_gif, fig_title, x_label, y_label, filename, gif_line_args);
+end
 
--- plot2gif.m
+++ plot2gif.m
@@ -1,16 +1,20 @@
 % Creates a new animated figure from data, displays it and saves it to a gif.
 function  plot2gif(fig_num, x, y, fig_title, x_label, y_label, output_filename, plot_style) 
     f = figure(fig_num);
-    axis tight manual;
-    grid on;
+    line = animatedline(x(1), y(1), plot_style{:});
+    hold on;
     
-    title(fig_title);
-    xlabel(x_label);
-    ylabel(y_label);
+    x_lim = [min(x), max(x)];
+    y_lim = [min(y), max(y)];  
 
     for i  = 1:numel(x)
-        
-        plot(x(i), y(i), plot_style{:});
+        addpoints(line, x(i), y(i))
+        title(fig_title);
+        xlim(x_lim);
+        ylim(y_lim);
+        xlabel(x_label);
+        ylabel(y_label);    
+        grid on;
         drawnow;
         
         im = frame2im(getframe(f));
@@ -20,6 +24,6 @@ function  plot2gif(fig_num, x, y, fig_title, x_label, y_label, output_filename, 
         else
             imwrite(imind, cm, output_filename, "gif", "WriteMode", "append" );
         end
-
     end
+    hold off;
 end
